1ae7b0ddd7aa7e29ca7bc7850e3df5909675dd0a
surazz999 <surazsub999@gmail.com>
2023-02-17 15:12:33 +0545

    listview

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,36 +1,31 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  List<String> products = ["Bed", "Sofa", "Chair"];
+  List<String> ProductDetails = [
+    "king size bed",
+    "king size sofa",
+    "wooden chair"
+  ];
+  List<int> price = [4000, 5000, 508];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: OutlinedButton(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("let's begin"),
-              Icon(Icons.add_shopping_cart_outlined),
-            ],
-          ),
-          // icon: Icon(Icons.add_shopping_cart_outlined),
-          // label: Text("let's begin"),
-          onPressed: () {},
-          style: ElevatedButton.styleFrom(
-              padding: EdgeInsets.all(20.0),
-              fixedSize: Size(300, 80),
-              textStyle: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              // primary: Color.fromARGB(255, 255, 230, 0),
-              onPrimary: Colors.black87,
-              // elevation: 15,
-              shadowColor: Colors.yellow,
-              // side: BorderSide(color: Colors.black87,width: 2),
-              // alignment: Alignment.bottomLeft,
-              shape: StadiumBorder()),
+      body: Container(
+        child: ListView.builder(itemCount: products.length,
+          itemBuilder: (context, index){
+            return ListTile(
+              leading: CircleAvatar(child: Text(products[index][0]),),
+              title: Text(products[index]),
+              subtitle: Text(ProductDetails[index]),
+              trailing: Text(price[index].toString()),
+            );
+          }),
         ),
-      ),
-    );
+      );
+    
   }
 }
